5f4c8fad5d643369357fb967768ec3cb23ac1656
ZMAN <zachh@github.com>
2021-01-27 17:13:51 -0600

    contact form and rendering

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -6,7 +6,25 @@ import Nav from '..';
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
+it('renders', () => {
+  render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
+})
+
+describe('Nav component renders', () => {
   it('renders', () => {
     render(<Nav />);
   });
